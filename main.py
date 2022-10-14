4fad3d4cfeb9e14dfdb512f57ed91983321640ac
butsevden <butsevden@mail.ru>
2022-10-14 13:01:41 +0300

    333

--- main.py
+++ main.py
@@ -8,33 +8,14 @@ import json
 
 def get_url():
     cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
-    url = 'https://www.labirint.ru/genres/2308/'
-    source = url.split('/')[-2]
-    print(source)
+    soup, header = get_html_text()
+    genre = soup.find('h1', class_='genre-name').text
 
-    with open(f'labitint_{cur_time}_{source}.csv', 'w', encoding='utf-8') as file:
-        writer = csv.writer(file)
+    save_csv(cur_time, genre)
 
-        writer.writerow(
-            (
-                'Название книги',
-                'Автор книги',
-                'Издательство',
-                'Цена',
-                'Скидка на книгу',
-                'Наличие'
-            )
-        )
+    soup, header = get_html_text()
 
-    url = 'https://www.labirint.ru/genres/2308/'
-    header = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.2.1495 Yowser/2.5 Safari/537.36'
-    }
-    responce = requests.get(url=url, headers=header).text
-    soup = BeautifulSoup(responce, 'html.parser')
     page_count = int(soup.find('div', class_='pagination-numbers').find_all('a')[-1].text)
-    book_count = 0
     books_data = []
 
 
@@ -79,7 +60,7 @@ def get_url():
                     'book_cell': book_cell
                 }
             )
-            with open(f'labitint_{cur_time}_{source}.csv', 'a', encoding='utf-8') as file:
+            with open(f'labitint_{cur_time}_{genre}.csv', 'a', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow(
                     (
@@ -90,13 +71,49 @@ def get_url():
                         book_cell
                     )
                 )
-            book_count+=1
-
         print(str(page) + f'/{page_count}-страница')
         time.sleep(1)
-    with open(f'labirint_{cur_time}_{source}.json', 'w', encoding='utf-8') as file:
+    save_json(books_data, cur_time, genre)
+
+
+def get_html_text():
+    url = 'https://www.labirint.ru/genres/2308/'
+    header = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.2.1495 Yowser/2.5 Safari/537.36'
+    }
+    responce = requests.get(url=url, headers=header).text
+    soup = BeautifulSoup(responce, 'html.parser')
+    return soup, header
+
+
+def save_json(books_data, cur_time, genre):
+    url = 'https://www.labirint.ru/genres/2308/'
+    header = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.2.1495 Yowser/2.5 Safari/537.36'
+    }
+    responce = requests.get(url=url, headers=header).text
+    soup = BeautifulSoup(responce, 'html.parser')
+    return soup, header
+    with open(f'labirint_{cur_time}_{genre}.json', 'w', encoding='utf-8') as file:
         json.dump(books_data, file, indent=4, ensure_ascii=False)
-    print(book_count)
+
+
+def save_csv(cur_time, genre):
+    with open(f'labitint_{cur_time}_{genre}.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                'Название книги',
+                'Автор книги',
+                'Издательство',
+                'Цена',
+                'Скидка на книгу',
+                'Наличие'
+            )
+        )
 
 
 if __name__ == '__main__':
